3a02f5d87c7b179e7ff8356351714cbda0439423
David <anddagger@gmail.com>
2022-08-16 20:23:44 +0000

    cleaned up code

---
+++ html-css-design-and-build-websites/src/js/forms.js
@@ -0,0 +1,13 @@
+const submitPersonal = document.getElementById('submit-personal')
+
+function validate(email) {
+  const email = document.getElementById('email-box')
+  const pattern = /^[^]+@[^]+\.[a-z]{2,3}$/
+
+  if (input.value.match(pattern)) {
+    email.classList.add('valid')
+    email.classList.remove('invalid')
+  }
+}
+
+submitPersonal.addEventListener('click', validate(email))
---
+++ html-css-design-and-build-websites/src/js/main.js
@@ -0,0 +1,6 @@
+function scrollToBottom() {
+  window.scrollTo(0, document.body.scrollHeight)
+}
+
+history.scrollRestoration = 'manual'
+window.onload = scrollToBottom
